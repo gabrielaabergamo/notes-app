296443f0365ab973a805606dce1e9bb684a6952f
gabrielaabergamo <gabiibergamo@gmail.com>
2021-10-28 16:59:29 -0300

    podemos adicionar notas

--- notes.js
+++ notes.js
@@ -1,9 +1,33 @@
+const fs = require('fs')
+
 function getNotes(){
     return('Your notes...')
 }
 
 function addNotes(title, body){
-    //corpo
+    
+    const notes = loadNotes()
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
+}
+
+function saveNotes(notes){
+    const aux = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', aux)
+}
+
+function loadNotes(){
+    try{
+        const data = fs.readFileSync('notes.json')
+        return JSON.parse(data)
+    } catch(e){
+        return []
+    }  
 }
 
 module.exports = {
