62711e9a8826ae770d3473873a6abc4ed7465a8e
gabrielaabergamo <gabiibergamo@gmail.com>
2021-10-28 16:41:16 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const chalk = require('chalk')
+const notes = require('./notes.js')
+const yargs = require('yargs')
+const { demandOption } = require('yargs')
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'add new note',
+    builder: {
+        title:{  
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+
+    handler: function(argv){
+        notes.addNotes(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function(){
+        console.log('removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'listing notes',
+    handler: function(){
+        console.log('listed notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'reading notes',
+    handler: function(){
+        console.log('reading the notes')
+    }
+})
+yargs.parse()
+// console.log(yargs.argv)
---
+++ notes.js
@@ -0,0 +1,12 @@
+function getNotes(){
+    return('Your notes...')
+}
+
+function addNotes(title, body){
+    //corpo
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNotes
+}
